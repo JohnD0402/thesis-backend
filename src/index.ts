0b33cd190059bb0cd0bfe85b44b9bf73d95b645d
John Daria <johndaria0209@gmail.com>
2024-12-01 16:18:50 +0800

    update for api

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+
+let data = {
+  temperature: "",
+  humidity: "",
+};
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/postDeviceReadings", (request: Request, response: Response) => {
+  const { temperature, humidity } = request.body;
+
+  data = {
+    temperature,
+    humidity,
+  };
+  response.status(200).send("ok");
+});
+
+app.get("/getDeviceReadings", (request: Request, response: Response) => {
+  response.status(200).send(data);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+export default app;
