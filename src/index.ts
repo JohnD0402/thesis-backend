6c1819b532e5f6fa6171f6e3d62c3d6a0db61642
John Daria <johndaria0209@gmail.com>
2024-12-04 14:39:04 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import cors from "cors";
 let data = {
   temperature: "",
   humidity: "",
+  lightBlock: "",
+  rainValue: "",
+  lux: "",
+  control: {
+    window: false,
+    waterPump: false,
+  },
 };
 
 const app = express();
@@ -11,17 +18,49 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/postDeviceReadings", (request: Request, response: Response) => {
-  const { temperature, humidity } = request.body;
+  const { temperature, humidity, lightBlock, rainValue, lux } = request.body;
 
   data = {
+    ...data,
     temperature,
     humidity,
+    lightBlock,
+    rainValue,
+    lux,
   };
   response.status(200).send("ok");
 });
 
 app.get("/getDeviceReadings", (request: Request, response: Response) => {
   response.status(200).send(data);
+});
+
+app.post("/control/window", (request: Request, response: Response) => {
+  const { window } = request.body;
+
+  data = {
+    ...data,
+    control: {
+      ...data.control,
+      window,
+    },
+  };
+
+  response.status(200).send("ok");
+});
+
+app.post("/control/water_pump", (request: Request, response: Response) => {
+  const { waterPump } = request.body;
+
+  data = {
+    ...data,
+    control: {
+      ...data.control,
+      waterPump,
+    },
+  };
+
+  response.status(200).send("ok");
 });
 
 app.listen(3000, () => {
